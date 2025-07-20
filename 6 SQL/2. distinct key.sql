da8a9e2f3bd2b23be720b4c99425437510508ddd
pratik1404 <patilpratikmanohar@gmail.com>
2025-07-20 17:37:50 +0530

    Sql

---
+++ 6 SQL/2. distinct key.sql
@@ -0,0 +1,7 @@
+use students;
+
+select distinct name from student ;
+
+select distinct grade from student ;
+
+select distinct * from student ;
---
+++ 6 SQL/temp.sql
@@ -0,0 +1,5 @@
+create temporary table temp1 as 
+select * from student ;
+
+
+select * from temp1
