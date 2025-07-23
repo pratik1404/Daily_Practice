10804185c9318009c67692dba94f9b2e894053c1
pratik Patil <143781785+pratik1404@users.noreply.github.com>
2025-07-23 17:18:49 +0530

    Add files via upload

---
+++ 6 SQL/7_null_operator.sql
@@ -0,0 +1,33 @@
+-- insert into employees
+-- values
+
+select * from employees
+
+
+insert into employees
+values 
+(6, 'vedant' , 'gore' , 'product developer' , 80000 , '2025-01-20')
+
+select * from Employees
+
+insert into employees
+(id , firstname , lastname )
+values
+(7, 'nezuko' , 'kamado' )
+
+insert into employees
+values
+(8, 'tenjan' , 'uzui' , '', 54000 ,  '2025-01-20')
+
+select * from Employees
+
+insert into employees
+values
+(9, 'muzan' , 'kibutsuji' , 0 , 54000 ,  '2025-01-20')
+
+
+select * from employees where department is null
+
+select * from Employees wheRe dEpartment is not null
+
+ 
